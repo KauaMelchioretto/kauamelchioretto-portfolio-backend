7668cd98025d09d51d51e292342df570ae58b545
Kauã Melchioretto <kauamelchoretto@gmail.com>
2022-08-14 16:28:02 -0300

    test new configs

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.listen(process.env.PORT || 3001, () => {
 
 const contactEmail = nodemailer.createTransport({
     host: "outlook.office365.com",
-    port: 587,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
