04dfc45913543677b42f69961bd7cc8dfffb601d
Kauã Melchioretto <kauamelchoretto@gmail.com>
2022-08-14 17:13:14 -0300

    try new configs

--- server.js
+++ server.js
@@ -9,45 +9,50 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`Server running in port ${process.env.PORT}`);
-  });
+  console.log(`Server running in port ${process.env.PORT}`);
+});
 
 const contactEmail = nodemailer.createTransport({
-    service: "outlook365",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.live.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    ciphers: "SSLv3",
+  },
 });
 
 contactEmail.verify((error) => {
-    if (error) {
-        console.log("Error: " + error);
-    } else {
-        console.log("Ready to Send.");
-    }
-})
+  if (error) {
+    console.log("Error: " + error);
+  } else {
+    console.log("Ready to Send.");
+  }
+});
 
 router.post("/contact", (request, response) => {
-    const { name } = request.body;
-    const { email } = request.body;
-    const { message } = request.body;
-    const { phone } = request.body;
-    const mail = {
-        from: name,
-        to: "kauamelchioretto@outlook.com",
-        subject: "Contact Form Submission - Portfolio",
-        html: `<p>Name: ${name} </p>
+  const { name } = request.body;
+  const { email } = request.body;
+  const { message } = request.body;
+  const { phone } = request.body;
+  const mail = {
+    from: name,
+    to: "kauamelchioretto@outlook.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name} </p>
         <p>Email: ${email} </p>
         <p>Phone: ${phone} </p>
         <p>Message: ${message} </p>`,
-    };
+  };
 
-    contactEmail.sendMail(mail, (error) => {
-        if (error){
-            response.send(error);
-        } else {
-            response.send({code: 200, status: "Message Sent"});
-        }
-    });
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      response.send(error);
+    } else {
+      response.send({ code: 200, status: "Message Sent" });
+    }
+  });
 });
