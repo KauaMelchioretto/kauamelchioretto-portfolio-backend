f6619f1f2198d79accbccd9256c004b78046372d
Kauã Melchioretto <kauamelchoretto@gmail.com>
2022-08-14 14:28:44 -0300

    add .env configs and server url

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3001, () => console.log("Server running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server running in port ${process.env.PORT}`);
+  });
 
 const contactEmail = nodemailer.createTransport({
     service: "outlook",
     auth: {
-        user: "kauamelchioretto@outlook.com",
-        pass:   "Sandro123",
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     },
 });
 
