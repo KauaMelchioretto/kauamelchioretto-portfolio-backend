c92916aedd6917609541f9f6d7a7d7a1a78e1101
Kauã Melchioretto <kauamelchoretto@gmail.com>
2022-08-14 17:21:33 -0300

    secure false in outlook.office

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.listen(process.env.PORT || 3001, () => {
 
 const contactEmail = nodemailer.createTransport({
   host: "outlook.office365.com",
-  secure: true,
+  secure: false,
   port: 587,
   auth: {
     user: process.env.EMAIL_USER,
